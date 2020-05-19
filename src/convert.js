76d964c3af0472a215ea1e03de15d7fce4b1aab2
Kuan Cheang <kuancheang@asqimacau.com>
2020-05-19 19:23:38 +0800

    Added markdown metadata into generated html.

---
+++ src/convert.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const showdown = require('showdown')
+const htmlMinifier = require('html-minifier').minify
+const pretty = require('pretty');
+const default_template = 'asqi-glp.html'
+const default_theme = 'github-theme.css'
+
+async function mdToHtml(mdContent, template = default_template, theme = default_theme, metadata = {
+  title: 'Project title',
+  author: 'Your name',
+  version: 'v1.0'
+}) {
+  const themePath = `${__dirname}/themes/${theme}`
+  let themeData = ''
+  const templatePath = `${__dirname}/templates/${template}`
+  let templateData = ''
+  // initialize a mardown converter
+  const converter = new showdown.Converter({
+    ghCompatibleHeaderId: true,
+    simpleLineBreaks: true,
+    ghMentions: true,
+    tables: true,
+  })
+  converter.setFlavor('github')
+
+  try {
+    // Checking the content is exist on the theme file and load content into variable
+    themeData = fs.readFileSync(themePath).toString()
+    if (!themeData) {
+      throw `No content found on the theme file at path "${themePath}"`
+    }
+    // Checking the content is exist on the template file and load content into variable
+    templateData = fs.readFileSync(templatePath).toString()
+    if (!templateData) {
+      throw `No content found on the template file at path "${templatePath}"`
+    }
+  } catch (error) {
+    console.log(error);
+    return ''
+  }
+  // substitute the template variable with actual content
+  templateData = templateData.replace(/{{doc_title}}/g, metadata.title)
+  templateData = templateData.replace(/{{doc_version}}/g, metadata.version)
+  templateData = templateData.replace(/{{doc_author}}/g, metadata.author)
+  templateData = templateData.replace(/{{doc_date}}/g, (new Date).toLocaleDateString())
+  templateData = templateData.replace(/{{doc_style}}/g, themeData)
+  templateData = templateData.replace(/{{doc_content}}/g, converter.makeHtml(mdContent))
+  // pretty the html since markdown is not with same indent depth as that of template
+  templateData = pretty(templateData)
+
+  const htmlContent = htmlMinifier(templateData, {
+    html5: true,
+    removeComments: true,
+    minifyCSS: true,
+    minifyJS: true
+  });
+  return htmlContent;
+}
+
+module.exports = {
+  mdToHtml
+};
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const path = require('path')
+const { mdToHtml } = require('./convert')
+
+let infile = ''
+let outfile = ''
+const argv = process.argv
+
+if (argv.length < 3) {
+  console.log('Missing argument for input markdown file.')
+  return
+} else if (argv.length < 4) {
+  console.log('Missing argument for output file.')
+  return
+} else {
+  infile = path.resolve(argv[2])
+  outfile = path.resolve(argv[3])
+
+  if (!infile) {
+    console.log('Markdown file is not found')
+    return
+  }
+}
+
+const capitialize = (txt) => {
+  return txt
+    .split(' ')
+    .map((s) => s[0].toUpperCase() + s.substring(1, s.length))
+    .join(' ')
+}
+
+
+try {
+  metadata = {
+    title: 'My project',
+    author: 'Your name',
+    version: 'v1.0',
+  }
+  const mdContent = fs.readFileSync(infile).toString()
+  const matches = mdContent.match(/\[comment\]:\s*#\s*\((.*:.*)\)/g)
+  // extract the document information from the markdown
+  // [comment]: # (title : <Your Project Name>)
+  // [comment]: # (author : <Your Name>)
+  // [comment]: # (version : <Document Version>)
+  if (matches.length > 0) {
+    const regx = /\[comment\]:\s*#\s*\(.*:(.*)\)/
+    for (let i = 0; i < matches.length; i++) {
+      const pattern = matches[i]
+      const norm = pattern.trim().toLowerCase()
+      if (norm.indexOf('title') > -1) {
+        // Capitialize each word for the title
+        metadata.title = capitialize(norm.replace(regx, '$1').trim())
+      } else if (norm.indexOf('author') > -1) {
+        metadata.author = capitialize(norm.replace(regx, '$1').trim())
+      } else if (norm.indexOf('version') > -1) {
+        metadata.version = norm.replace(regx, '$1').trim()
+      }
+    }
+  }
+
+  mdToHtml(mdContent, undefined, undefined, metadata).then((htmlContent) => {
+    fs.writeFile(
+      outfile,
+      htmlContent,
+      { encoding: 'utf8', flag: 'w' },
+      (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('Converted HTML file is generated at ' + outfile)
+        }
+      }
+    )
+  })
+} catch (err) {
+  console.log(err)
+  return
+}
