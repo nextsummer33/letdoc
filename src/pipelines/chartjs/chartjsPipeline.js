244479211e292b0d800641aa5ad5d53d0883874a
Kuan Cheang <kuancheang@asqimacau.com>
2020-05-26 15:18:03 +0800

    Resize the canvas based on the width and height

--- src/pipelines/chartjs/chartjsPipeline.js
+++ src/pipelines/chartjs/chartjsPipeline.js
@@ -7,38 +7,45 @@ async function chartjsPipeline(
     config,
     width: 900,
     height: 400,
-    deviceScaleFactor: 1,
+    deviceScaleFactor: 1
   }
 ) {
   const { config, width, height, deviceScaleFactor } = options
   // Get all the context code block for chartjs in the markdown content
-  const context = mdContent.match(/```chartjs\n*([^`]+)\n*```/g)
+  const context = mdContent.match(/```js\schartjs\n*([^`]+)\n*```/g)
 
   if (context && context.length) {
-    // Running a puppeteer and headless chromiumn
+    // Running a puppeteer and headless chromimun
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     page.setViewport({ width, height, deviceScaleFactor })
     await page.goto(`file://${path.join(__dirname, 'index.html')}`)
     await page.evaluate(`document.body.style.background = 'transparent'`)
+    await page.$eval('canvas', (canvas, width, height) => {
+      canvas.width = width;
+      canvas.height = height;
+    }, width, height)
 
     for (let i = 0; i < context.length; i++) {
-      // extract the context in the mermaid code block
-      const code = context[i].replace(/```chartjs\n*([^`]+)\n*```/, '$1')
-      const chartjsData = JSON.parse(code.replace(/'/g, '"'))
-
-      const imgData = await page.$eval('canvas', (canvas, data) => {
-        var ctx = canvas.getContext('2d')
-        var chart = new Chart(ctx, data)
+      // extract the context in the chartjs code block
+      const code = context[i].replace(/```js\schartjs\n*([^`]+)\n*```/, '$1')
+      // evaluate the code in the page
+      page.evaluate(code)
+      const imgData = await page.$eval('canvas', (canvas, shrink) => {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            resolve(chart.toBase64Image())
-          }, 1000);
+            let data = canvas.toDataURL()
+            if (data) {
+              resolve(data)
+            } else {
+              reject('Empty canvas context after running the chartjs')
+            }
+          }, 500);
         })
-      }, chartjsData)
+      })
 
       mdContent = mdContent.replace(
-        /```chartjs[^`]*```/,
+        /```js\schartjs[^`]*```/,
         `<div class="chartjs"><img src="${imgData}" /></div>`
       )
     }
